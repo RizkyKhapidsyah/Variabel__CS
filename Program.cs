2f7be14f4ed75823d7e1cdd0f8f37116e1267eca
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-12-19 07:22:36 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Variabel__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 42;             //Bilangan Bulat
+            double Pi = 3.14;       //Bilangan Desimal
+            char Y = 'Y';
+            string Hai = "Haloo..";
+            bool Kondisi = true;
+
+            Console.WriteLine(X);
+            Console.WriteLine(Pi);
+            Console.WriteLine(Y);
+            Console.WriteLine(Hai);
+
+            if (Kondisi = true)
+            {
+                X = 50;
+                Console.WriteLine(X);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
